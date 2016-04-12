797bf2100290312584edda449c9d075392abe95b
Lotzi Bölöni <lotzi.boloni@gmail.com>
2016-04-12 16:42:53 -0400

    Small fix for DisplayText

--- src/main/java/yaes/rcta/util/DisplayText.java
+++ src/main/java/yaes/rcta/util/DisplayText.java
@@ -4,23 +4,29 @@ import yaes.world.physical.location.IMoving;
 import yaes.world.physical.location.INamed;
 import yaes.world.physical.location.Location;
 
+/**
+ * An object which represents in a YAES diagram a changeable text representation
+ * shown in the visual field (usually, a label). 
+ * 
+ * @author lboloni
+ *
+ */
 public class DisplayText implements INamed, IMoving {
 
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 6636378942248601796L;
 	private Location displayLocation;
 	private String displayText;
-	public DisplayText(){
-		this.displayLocation = new Location(0,0);
+
+	public DisplayText() {
+		this.displayLocation = new Location(0, 0);
 		this.displayText = "Nothing";
 	}
-	public DisplayText(Location displayLocation){
+
+	public DisplayText(Location displayLocation) {
 		this.displayLocation = displayLocation;
 		this.displayText = "Nothing";
 	}
+
 	@Override
 	public Location getLocation() {
 		return displayLocation;
@@ -34,11 +40,16 @@ public class DisplayText implements INamed, IMoving {
 
 	@Override
 	public String getName() {
-		return this.displayText;
+		return displayLocation.toString();
+	}
+
+	public void setDisplayText(String text) {
+		this.displayText = text;
 	}
 	
-	public void setDisplayText(String text){
-		this.displayText = text;
+	@Override
+	public String toString() {
+		return "Display text at: " + displayLocation + "\n" + displayText;
 	}
 
 }
